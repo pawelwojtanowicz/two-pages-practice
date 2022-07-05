17f83266db1843d4512aef0cf5e8b82f3632ed15
pawelwojtanowicz <93117912+pawelwojtanowicz@users.noreply.github.com>
2022-07-05 08:51:21 +0200

    next step will be by brother

--- src/userify-components/UserList.tsx
+++ src/userify-components/UserList.tsx
@@ -16,9 +16,9 @@ const UserList: React.FC = ({ setShow, show, userPropsList }) => {
   return (
     <Box ml={3} textAlign="left">
       <Typography>User List</Typography>
-      <Box sx={{ bgcolor: "LightSteelBlue", width: 215, height: 70 }}>
-        {userPropsList}
-      </Box>
+
+      {userPropsList}
+
       <Button variant="contained" onClick={onChange}>
         Add (+)
       </Button>
--- src/userify-components/Users.tsx
+++ src/userify-components/Users.tsx
@@ -3,12 +3,16 @@ import MyAppBar from "./MyAppBar";
 import UserList from "./UserList";
 import AddingSpace from "./AddingSpace";
 import { useState } from "react";
+import { Box } from "@mui/material";
 
 const Users = () => {
   const [show, setShow] = useState(false);
   const [users, setUsers] = useState([]);
-  const labels = ["name: ", "last name: ", "age: "];
-  const userPropsList = users.map((user) => <li key="user">{user}</li>);
+  const userPropsList = users.map((user) => (
+    <Box sx={{ bgcolor: "LightSteelBlue", width: 215, height: 70 }}>
+      <li key="user">{user}</li>
+    </Box>
+  ));
   return (
     <div>
       This is Users page!
